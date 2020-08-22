ca0cad9dd0915ba8bdc230c3d376c594bbc1f4f6
Reba001 <aaperez.tomas@gmail.com>
2020-08-22 14:39:16 -0600

    Font awesome agregado y probado al 100

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { AsignacionComponent } from './components/asignacion/asignacion.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
 import { HomeComponent } from './components/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
