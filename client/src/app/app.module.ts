0b0fa213bff5d7dbe67766ea368aa2c8bda0bf77
Reba001 <aaperez.tomas@gmail.com>
2020-08-22 11:09:18 -0600

    Cambios a registro de usuario

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroUsuarioComponent } from './Components/registro-usuario/registro-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroUsuarioComponent
   ],
   imports: [
     BrowserModule,
