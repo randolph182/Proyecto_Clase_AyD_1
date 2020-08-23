248e1896270e2764896eb94c60dacde0c642f014
Inti <intirocks888@gmail.com>
2020-08-22 19:37:10 -0600

    Actualización modulos

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AsignacionComponent } from './components/asignacion/asignacion.component';
-import { HeaderPrincipalComponent } from './components/header-principal/header-principal.component';
 import { RegistroUsuarioComponent } from './Components/registro-usuario/registro-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AsignacionComponent,
-    HeaderPrincipalComponent,
     RegistroUsuarioComponent
   ],
   imports: [
