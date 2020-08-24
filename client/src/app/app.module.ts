d5f93e2b9cac2edb2e3fb987590aaf95bc420796
OscarJO <oscar.jordan.21091@gmail.com>
2020-08-24 07:51:37 -0600

    encabezado agregado

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,17 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AsignacionComponent } from './components/asignacion/asignacion.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 
-
+library.add(fas,far);
 @NgModule({
   declarations: [
     AppComponent,
     AsignacionComponent,
+    EncabezadoComponent,
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
