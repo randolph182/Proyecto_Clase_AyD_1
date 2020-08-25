7ed792717765602334040611fd7e962a9b56e1c8
OscarJO <oscar.jordan.21091@gmail.com>
2020-08-24 20:20:55 -0600

    diseño login mejorado

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AsignacionComponent } from './components/asignacion/asignacion.component';
 import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { LoginComponent } from './components/login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 library.add(fas,far);
 @NgModule({
@@ -16,12 +18,14 @@ library.add(fas,far);
     AppComponent,
     AsignacionComponent,
     EncabezadoComponent,
-    
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    loginForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
 
-  ngOnInit(): void {
-  }
+  constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+
+    
 
 }
+  get f() { return this.loginForm.controls; }
+
+  }
+
+
