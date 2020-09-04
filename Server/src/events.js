88da1294e5b411c3dd4ac607408b99f138a164fa
Alejandro <alerod620@gmail.com>
2020-09-03 19:58:50 -0600

    Obtención de cursos

--- Server/src/events.js
+++ Server/src/events.js
@@ -25,6 +25,12 @@ function createRouter(db) {
     );
   });
 
+/*
+********************************
+**********METODOS POST**********
+********************************
+*/
+
   //******Insertar escuela******
   router.post('/insertar_escuela', (req, res, next) => {
     db.query(
@@ -44,11 +50,30 @@ function createRouter(db) {
     );
   });
 
-//******Login******
-router.get('/login', (req, res, next) => {
+  //******Insertar escuela******
+  router.post('/registro', (req, res, next) => {
+    db.query(
+      'INSERT INTO ESCUELA(id_escuela, nombre) VALUES(?,?)',
+      [req.body.id, req.body.nombre],
+      (error) => {
+        if(error)
+        {
+          console.error(error);
+          res.status(500).json({status:'error'});
+        }
+        else
+        {
+          res.status(200).json({status:'ok'});
+        }
+      }
+    );
+  });
+
+  //******Obtener cursos para mostrarlos al momento de asignar******
+router.post('/obtener_seccion', (req, res, next) => {
   db.query(
-    'SELECT * FROM ESTUDIANTE_CUENTA WHERE login = ? AND password = ?',
-    [req.body.login, req.body.password],
+    'SELECT * FROM SECCION WHERE id_curso',
+    [req.body.curso],
     (error, results) => {
       if(error)
       {
@@ -63,7 +88,53 @@ router.get('/login', (req, res, next) => {
   );
 });
 
+/*
+*******************************
+**********METODOS GET**********
+*******************************
+*/
 
+//******Login******
+router.get('/login', (req, res, next) => {
+  if(req.body.rol = 'E')
+  {
+    db.query(
+      'SELECT * FROM ESTUDIANTE_CUENTA WHERE login = ? AND password = ?',
+      [req.body.login, req.body.password],
+      (error, results) => {
+        if(error)
+        {
+          console.error(error);
+          res.status(500).json({status:'error'});
+        }
+        else
+        {
+          res.status(200).json(results);
+        }
+      }
+    );
+  }
+  
+});
+
+//******Obtener cursos para mostrarlos al momento de asignar******
+router.get('/obtener_cursos', (req, res, next) => {
+  db.query(
+    'SELECT CU.nombre, SE.seccion, SA.numero, ED.nombre,CAT.nombre FROM CURSO CU INNER JOIN SECCION SE ON CU.id_curso = SE.id_curso INNER JOIN SALON SA ON SE.id_salon = SA.id_salon INNER JOIN EDIFICIO ED ON SA.id_edificio = ED.id_edificio INNER JOIN DETALLE_CAT_CURSO DC ON SE.id_seccion = DC.id_seccion INNER JOIN ASIG_CATEDRATICO ASGC ON DC.id_asig_cate = ASGC.id_asig_cate INNER JOIN CATEDRATICO CAT ON ASGC.id_catedratico = CAT.id_catedratico',
+    [req.body.login, req.body.password],
+    (error, results) => {
+      if(error)
+      {
+        console.error(error);
+        res.status(500).json({status:'error'});
+      }
+      else
+      {
+        res.status(200).json(results);
+      }
+    }
+  );
+});
 
   return router;
 }
