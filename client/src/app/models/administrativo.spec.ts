c6cdc876436870a47c2b19a595e88f392fd28c48
Reba001 <aaperez.tomas@gmail.com>
2020-09-03 18:55:41 -0600

    Agregado la clase administrativo

---
+++ client/src/app/models/administrativo.spec.ts
@@ -0,0 +1,7 @@
+import { Administrativo } from './administrativo';
+
+describe('Administrativo', () => {
+  it('should create an instance', () => {
+    expect(new Administrativo()).toBeTruthy();
+  });
+});
