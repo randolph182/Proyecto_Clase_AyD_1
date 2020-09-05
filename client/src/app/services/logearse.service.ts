39974498e4973b35ef20e2b47a614a5b3513711b
OscarJO <oscar.jordan.21091@gmail.com>
2020-09-04 20:10:36 -0600

    actualizacion servicio

--- client/src/app/services/logearse.service.ts
+++ client/src/app/services/logearse.service.ts
@@ -20,10 +20,10 @@ export class LogearseService {
 
   retorno: string;
 
-  private usuariourl = 'https://104.198.58.234:3000/login';
+  private usuariourl = 'http://3.85.52.106:3000/login';
   constructor(private http:HttpClient) { }
 
-  headers: HttpHeaders = new HttpHeaders({
+  /*headers: HttpHeaders = new HttpHeaders({
 
     "Content-Type": "application/json",
   })
@@ -39,15 +39,13 @@ export class LogearseService {
         {headers: this.headers}
         ).pipe();
         //localStorage.setItem("sesion", correo); // seteando variable de sesión con el nombre del usuario
-  }
+  }*/
 
   login2(correo: string, pass: string, rol: string)
   {
-    this.http.post<any>(this.usuariourl, {"correo": correo, "password": pass, "rol": rol }).subscribe(data => {
+    this.http.post<any>(this.usuariourl, {"correo": correo, "password": pass, "rol": rol }, httpOptions).subscribe(data => {
     this.retorno = data.id;
-    console.log('respuesta: ' + data.id);
-}
-)
+    console.log('respuesta: ' + data.id);})
   }
   
 
