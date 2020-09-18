edaa6d9d4cfa2c1870ee630e72c304dd560ae71e
Inti Andrés <intirocks8888@gmail.com>
2020-09-17 23:09:35 -0600

    programación de funciones agregar, eliminar,e enviar y obtener cursos

--- client/src/app/components/asignacion-congreso/asignacion-congreso.component.ts
+++ client/src/app/components/asignacion-congreso/asignacion-congreso.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import {CongresoService} from '../../Services/congreso.service'
 @Component({
   selector: 'app-asignacion-congreso',
   templateUrl: './asignacion-congreso.component.html',
@@ -8,18 +9,41 @@ import { Component, OnInit } from '@angular/core';
 export class AsignacionCongresoComponent implements OnInit {
   congresosAgregados:any = [];
   congresosDisponibles:any=[];
-  constructor() { }
+  constructor(private congresoService:CongresoService,
+    private router:Router) { }
 
   ngOnInit(): void {
-
+    this.getCongresos();
+  }
+  getCongresos(){
+    this.congresoService.getCongresos()
+    .subscribe(
+      res=>{
+        this.congresosDisponibles = res;
+      },
+      err=>{
+        console.error(err);
+      }
+    )
   }
   eliminar(id:string){
-
+    if(this.congresosAgregados.length>0){
+      this.congresosAgregados = this.congresosAgregados.filter(congreso=>congreso.nombre+congreso.id_congreso!=id);
+    }
   }
   agregar(id:string){
-
+    this.congresosDisponibles.forEach(congreso => {
+      if((congreso.nombre+congreso.id_congreso)==id){
+        this.congresosAgregados.push(congreso);
+      }
+    });
   }
   enviar(){
-
+    if(this.congresosAgregados.length>0){
+      this.congresoService.enviarCongresos(1,this.congresosAgregados);
+      return true;
+    }
+    return false;
+    
   }
 }
